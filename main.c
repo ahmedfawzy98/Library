8ba1ff6dc13e72fe7640cb7056385826034a65c4
mcmody00 <tigogirl50@gmail.com>
2017-12-01 05:21:13 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
 typedef struct{
     char title[256];
@@ -13,15 +14,51 @@ typedef struct{
     char ctg[32];
     }book;
 
-FILE* fpointer;
+FILE* f;
 
 void insert_book();
+void search(char*);
 
 int main()
 {
-    //book b1={"Programming","Ahmed","Eagle","1234567890123","11/09/2012",1000,500,"Computer science"};
-    fpointer=fopen("C:\\Users\\Mido\\Desktop\\K\\Programming\\Labs\\3\\Files\\Library\\Books.txt","a+");
-    insert_book();
+    /*int choice,pass,trials=0;
+    while(1){
+    printf("1.Book Management.\n2.Memeber Management.\n3.Administrator?\n4.Save and exit.");
+    scanf("%d",&choice);
+    switch(choice){
+        case 1:
+            printf("1.Insert a book.\n2.Search for a book.\n3.Add a copy.\n4.Delete book.\n5.Back to menu.\n");
+            scanf("%d",&choice);
+            break;
+        case 2:
+            printf("1.Register.\n2.Borrow.\n3.Back to menu.\n");
+            scanf("%d",&choice);
+            choice+=5;
+            break;
+        case 3:
+            printf("Password(1234):");
+            scanf("%d",&pass);
+            while(pass!=1234){
+                printf("Incorrect.\nPassword(1234):");
+                trials++;
+                if(trials==3){
+                    trials=0;
+                    break;
+                }
+            }
+            printf("1.Overdue books.\n2.Most popular books.\n3.Back to menu.");
+            scanf("%d",&choice);
+            choice+=8;
+            break;
+    }
+    if(choice==5||choice==8||choice==11){
+        continue;
+    }
+    }*/
+    f=fopen("DB\\Books.txt","a+");
+    char kstr[128]="olit";
+    search(kstr);
+
     return 0;
 }
 
@@ -33,11 +70,29 @@ void insert_book(){
     printf("ISBN:");gets(b0.isbn);
     printf("Date of publication:");gets(b0.dop);
     printf("Category:");gets(b0.ctg);
-    printf("#Copies:");scanf("%d *[^\n]",&b0.copies);
+    printf("#Copies:");scanf("%d",&b0.copies);
     printf("#RemainingCopies:");scanf("%d",&b0.remCopies);
-    //char* test[10];
-    //gets(test);
-    //scanf("%s %s %s %s %s %d %d %s",b0.title,b0.author,b0.publisher,b0.isbn,b0.dop,&b0.copies,&b0.remCopies,b0.ctg);
-    fprintf(fpointer,"%s,%s,%s,%s,%s,%d,%d,%s\n",b0.title,b0.author,b0.publisher,b0.isbn,b0.dop,b0.copies,b0.remCopies,b0.ctg);
+    fprintf(f,"%s,%s,%s,%s,%s,%d,%d,%s\n",b0.title,b0.author,b0.publisher,b0.isbn,b0.dop,b0.copies,b0.remCopies,b0.ctg);
 
 }
+
+void search(char* kstr){
+    int i,i2,check=0;
+    char temp[1024];
+    while(fgets(temp,1024,f)!= NULL){
+        for(i=0;i<strlen(temp);i++){
+            if(temp[i]==kstr[0]){
+                for(i2=1;i2<strlen(kstr);i2++){
+                    if(temp[i+i2]==kstr[i2]){
+                        check++;
+                    }
+                }
+            }
+            if(check==strlen(kstr)-1){
+                    printf("%s",temp);
+                    check=0;
+                }
+        }
+    }
+}
+
