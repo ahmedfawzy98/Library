4c263326ccb3c9247fddee3a27bd6cef58cc1735
ahmedfawzy98 <33888344+ahmedfawzy98@users.noreply.github.com>
2017-12-01 23:08:33 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,98 +1,170 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <string.h>
-
 typedef struct{
-    char title[256];
-    char author[128];
-    char publisher[128];
-    char isbn[18];
-    char dop[11];
-    int copies;
-    int remCopies;
-    char ctg[32];
-    }book;
+    char title[200];
+    char author[200];
+    char publisher[200];
+    char ibsn[50];
+    char dop[50];
+    char copies[50];
+    char remcopies[50];
+    char ctg[100];
+}book;
 
-FILE* f;
 
-void insert_book();
-void search(char*);
-
+void insrt_book(book b)
+{
+    FILE *f1=fopen("Books.txt","a");
+    printf("Enter the info of book\n");
+    printf("Title:");gets(b.title);
+    printf("Author:");gets(b.author);
+    printf("Publisher:");gets(b.publisher);
+    printf("ISBN:");gets(b.ibsn);
+    printf("Date of publishing:");gets(b.dop);
+    printf("Copies:");gets(b.copies);
+    printf("Remaining Copies:");gets(b.remcopies);
+    printf("Category:");gets(b.ctg);
+    fprintf(f1,"%s,%s,%s,%s,%s,%s,%s,%s,\n,",b.title,b.author,b.publisher,b.ibsn,b.dop,b.copies,b.remcopies,b.ctg);
+    fclose(f1);
+}
+void search_book_title(char a[50])
+{
+    FILE *f1=fopen("Books.txt","r+");
+    char x[1000],y[100];
+    x[0]='\0';
+    fseek(f1,0,0);
+    fscanf(f1,"%[^\,]",x);
+    while(!feof(f1)){
+        if(strncmp(x,a,strlen(a))==0){
+               printf("Book info:\n");
+               fseek(f1,-strlen(x),1);
+               fscanf(f1,"%[^\,]",y);printf("Title: %s\n",y);fseek(f1,1,1);
+               fscanf(f1,"%[^\,]",y);printf("Author: %s\n",y);fseek(f1,1,1);
+               fscanf(f1,"%[^\,]",y);printf("Publisher: %s\n",y);fseek(f1,1,1);
+               fscanf(f1,"%[^\,]",y);printf("ISBN: %s\n",y);fseek(f1,1,1);
+               fscanf(f1,"%[^\,]",y);printf("Date of publishing: %s\n",y);fseek(f1,1,1);
+               fscanf(f1,"%[^\,]",y);printf("Copies: %s\n",y);fseek(f1,1,1);
+               fscanf(f1,"%[^\,]",y);printf("Remaining Copies: %s\n",y);fseek(f1,1,1);
+               fscanf(f1,"%[^\,]",y);printf("Category: %s",y);fseek(f1,1,1);
+               break;
+        }
+              x[0]='\0';
+              fseek(f1,1,1);
+              fscanf(f1,"%[^\,]",x);
+  }
+    if(strcmp(x,a)==1){
+               printf("Book is not found ");
+    }
+    fclose(f1);
+}
+void search_book_isbn(char a[50])
+{
+    FILE *f1=fopen("Books.txt","r+");
+    char x[1000],y[1000],z[100];
+    x[0]='\0';
+    y[0]='\0';
+    fseek(f1,0,0);
+    fscanf(f1,"%[^\,]",x);
+    strcat(y,x);
+    while(!feof(f1)){
+        if(strcmp(x,a)==0){
+                int c=3+strlen(y);
+               printf("Book info:\n");
+               fseek(f1,-c,1);
+               fscanf(f1,"%[^\,]",z);printf("Title: %s\n",z);fseek(f1,1,1);
+               fscanf(f1,"%[^\,]",z);printf("Author: %s\n",z);fseek(f1,1,1);
+               fscanf(f1,"%[^\,]",z);printf("Publisher: %s\n",z);fseek(f1,1,1);
+               fscanf(f1,"%[^\,]",z);printf("ISBN: %s\n",z);fseek(f1,1,1);
+               fscanf(f1,"%[^\,]",z);printf("Date of publishing: %s\n",z);fseek(f1,1,1);
+               fscanf(f1,"%[^\,]",z);printf("Copies: %s\n",z);fseek(f1,1,1);
+               fscanf(f1,"%[^\,]",z);printf("Remaining Copies: %s\n",z);fseek(f1,1,1);
+               fscanf(f1,"%[^\,]",z);printf("Category: %s",z);fseek(f1,1,1);
+               break;
+        }
+              x[0]='\0';
+              fseek(f1,1,1);
+              fscanf(f1,"%[^\,]",x);
+              strcat(y,x);
+              if(x[0]=='\n'){y[0]='\0';}
+  }
+    if(strcmp(x,a)!=0){
+               printf("Book is not found ");
+    }
+    fclose(f1);
+}
+void search_book_author(char a[50])
+{
+    FILE *f1=fopen("Books.txt","r+");
+    char x[1000],y[1000],z[100];
+    x[0]='\0';
+    y[0]='\0';
+    fseek(f1,0,0);
+    fscanf(f1,"%[^\,]",x);
+    strcat(y,x);
+    while(!feof(f1)){
+        if(strcmp(x,a)==0){
+                int c=1+strlen(y);
+               printf("Book info:\n");
+               fseek(f1,-c,1);
+               fscanf(f1,"%[^\,]",z);printf("Title: %s\n",z);fseek(f1,1,1);
+               fscanf(f1,"%[^\,]",z);printf("Author: %s\n",z);fseek(f1,1,1);
+               fscanf(f1,"%[^\,]",z);printf("Publisher: %s\n",z);fseek(f1,1,1);
+               fscanf(f1,"%[^\,]",z);printf("ISBN: %s\n",z);fseek(f1,1,1);
+               fscanf(f1,"%[^\,]",z);printf("Date of publishing: %s\n",z);fseek(f1,1,1);
+               fscanf(f1,"%[^\,]",z);printf("Copies: %s\n",z);fseek(f1,1,1);
+               fscanf(f1,"%[^\,]",z);printf("Remaining Copies: %s\n",z);fseek(f1,1,1);
+               fscanf(f1,"%[^\,]",z);printf("Category: %s",z);fseek(f1,1,1);
+               break;
+        }
+              x[0]='\0';
+              fseek(f1,1,1);
+              fscanf(f1,"%[^\,]",x);
+              strcat(y,x);
+              if(x[0]=='\n'){y[0]='\0';}
+  }
+    if(strcmp(x,a)!=0){
+               printf("Book is not found ");
+    }
+    fclose(f1);
+}
+void search_book_ctg(char a[50])
+{
+    FILE *f1=fopen("Books.txt","r+");
+    int i,counter=0;
+    char x[1000],y[1000];
+    char* z[1000];
+    x[0]='\0';
+    y[0]='\0';
+    fseek(f1,0,0);
+    fscanf(f1,"%[^\,]",x);
+    strcat(y,x);
+    while(!feof(f1)){
+        if(strcmp(x,a)==0){
+                int c=7+strlen(y);
+                fseek(f1,-c,1);
+                z[counter]=(char*)malloc(50);
+                fscanf(f1,"%[^\,]",z[counter]);
+                counter++;
+        }
+              x[0]='\0';
+              fseek(f1,1,1);
+              fscanf(f1,"%[^\,]",x);
+              strcat(y,x);
+              if(x[0]=='\n'){y[0]='\0';}
+  }
+  printf("Books in category (%s):\n",a);
+  for(i=0 ; i<=counter ; i++){printf("%s\n",z[i]);}
+    fclose(f1);
+}
 int main()
 {
-    /*int choice,pass,trials=0;
-    while(1){
-    printf("1.Book Management.\n2.Memeber Management.\n3.Administrator?\n4.Save and exit.");
-    scanf("%d",&choice);
-    switch(choice){
-        case 1:
-            printf("1.Insert a book.\n2.Search for a book.\n3.Add a copy.\n4.Delete book.\n5.Back to menu.\n");
-            scanf("%d",&choice);
-            break;
-        case 2:
-            printf("1.Register.\n2.Borrow.\n3.Back to menu.\n");
-            scanf("%d",&choice);
-            choice+=5;
-            break;
-        case 3:
-            printf("Password(1234):");
-            scanf("%d",&pass);
-            while(pass!=1234){
-                printf("Incorrect.\nPassword(1234):");
-                trials++;
-                if(trials==3){
-                    trials=0;
-                    break;
-                }
-            }
-            printf("1.Overdue books.\n2.Most popular books.\n3.Back to menu.");
-            scanf("%d",&choice);
-            choice+=8;
-            break;
-    }
-    if(choice==5||choice==8||choice==11){
-        continue;
-    }
-    }*/
-    f=fopen("DB\\Books.txt","a+");
-    char kstr[128]="olit";
-    search(kstr);
-
+    ///FILE *f1=fopen("Books.txt","r+");
+    ///book b1;
+    ///insrt_book(b1);
+    char a[50],b[50];
+    gets(a);///gets(b);
+    search_book_ctg(a);
+    ///Add_new_copy(a,b);
     return 0;
 }
-
-void insert_book(){
-    book b0;
-    printf("Title:");gets(b0.title);
-    printf("Author:");gets(b0.author);
-    printf("Publisher:");gets(b0.publisher);
-    printf("ISBN:");gets(b0.isbn);
-    printf("Date of publication:");gets(b0.dop);
-    printf("Category:");gets(b0.ctg);
-    printf("#Copies:");scanf("%d",&b0.copies);
-    printf("#RemainingCopies:");scanf("%d",&b0.remCopies);
-    fprintf(f,"%s,%s,%s,%s,%s,%d,%d,%s\n",b0.title,b0.author,b0.publisher,b0.isbn,b0.dop,b0.copies,b0.remCopies,b0.ctg);
-
-}
-
-void search(char* kstr){
-    int i,i2,check=0;
-    char temp[1024];
-    while(fgets(temp,1024,f)!= NULL){
-        for(i=0;i<strlen(temp);i++){
-            if(temp[i]==kstr[0]){
-                for(i2=1;i2<strlen(kstr);i2++){
-                    if(temp[i+i2]==kstr[i2]){
-                        check++;
-                    }
-                }
-            }
-            if(check==strlen(kstr)-1){
-                    printf("%s",temp);
-                    check=0;
-                }
-        }
-    }
-}
-
